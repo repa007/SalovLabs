4ed095ba43965d0c93b375f3e137a5e1441c77b6
repa007 <repatopt@mail.ru>
2021-09-18 14:48:06 +0300

    update1

---
+++ Lab1/pervoe.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 6;
+	int b = 3;
+	int c = 471;
+	int d = 0;
+	cout << "print integer\n";
+	cin >> d;
+	int Result = 0;
+	Result = a + c ^ b - d; //сначала возведение в степень
+	cout << "\n\n(>^.^)>  " << Result << endl << endl << endl << endl;
+}
---
+++ Lab1/vtoroe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	//объ€вл€ем переменные
+	int a = 6;
+	float b = 16.565;
+	string c = "5.3";
+
+
+	//преобразуем типы...
+	a = double(a);
+	b = double(b);
+	double cc = stod(c); //зафигачили строку как дабл
+	double Result = a + cc / b;
+
+	cout << "(>^.^)> " << Result << endl << endl << endl;
+}
