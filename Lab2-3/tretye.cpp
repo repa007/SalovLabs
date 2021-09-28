9ffa06684ba0cf13968953ccaabebd406cb4b3cd
repa007 <repatopt@mail.ru>
2021-09-28 15:20:42 +0300

    Lab2-3

---
+++ Lab2-3/tretye.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{ 
+    int x, y, n;
+    cout << "Enter x\n";
+    cin >> x;
+    y = 0;
+    n = 0;
+    while (y < 1000)
+    {
+        y = y + x;
+        n = n + 1;
+    }
+    cout << "n = " << n <<endl;
+    system("pause");
+    return 0;
+}
---
+++ Lab2-3/vtoroe.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Enter x\n";
+    cin >> x;
+    y = 2 * x * x + x + 1;
+    if (y > 777)
+    {
+        cout << "(^.^)\n\n";
+    }
+    else
+    {
+        cout << "(0_o)\n\n";
+    }
+    system("pause");
+    return 0;
+}
