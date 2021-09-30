1735d658fa78aedb717b62302480d7367a202a77
repa007 <50541383+repa007@users.noreply.github.com>
2021-09-30 13:00:49 +0300

    Update pervoe.cpp

--- Lab2-3/pervoe.cpp
+++ Lab2-3/pervoe.cpp
@@ -1,21 +1,13 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
-
 int main()
 {
-    int x, y;
-    cout << "Enter x\n";
-    cin >> x;
-    y = 2 * x * x + x + 1;
-    if (y > 777)
-    {
-        cout << "(^.^)\n\n";
-    }
-    else
-    {
-        cout << "(0_o)\n\n";
-    }
-    system("pause");
-    return 0;
+ int x, y;
+ cout << "Enter x\\n";
+ cin >> x;
+ y = 2 * x * x + x + 1;
+ cout << "y = " << y;
+system("pause");
+ return 0;
 }
