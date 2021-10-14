16053d83f7994b5a8ead321c021623b4cac68285
repa007 <repatopt@mail.ru>
2021-10-14 13:11:33 +0300

    Create main.cpp

---
+++ Lab4-5/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int  a;
+
+    //заполнение массива
+    const int len = 10;
+    int arr[len];
+    for (int i = 0; i <= len - 1; i++) { cin >> arr[i]; }
+
+    // Сортировка массива
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                //переворот пар
+                a = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = a;
+            }
+        }
+    }
+
+    // Вывод
+    cout << "\n\n";
+    for (int i = 0; i < len; i++) {
+        cout << arr[i] << " ";
+    }
+    
+
+    system("pause");
+    return 0;
+}
