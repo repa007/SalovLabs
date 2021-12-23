8ad8fbbb4a862478dfa1203b52b0507c85c8ad43
repa007 <repatopt@mail.ru>
2021-12-23 12:37:48 +0300

    Create main.cpp

---
+++ Lab18/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <chrono>
+#include <iostream>
+#include <ctime>  
+
+using namespace std;
+
+void bubbleSort(int* x, int size) { // собсна пузырёчек
+	auto begin = std::chrono::steady_clock::now(); // начальное время
+	for (int i = 0; i < size; i++) {
+		for (int n = 0; n < size - 1; n++) {
+			if (x[n] > x[n + 1]) {
+				int y = x[n];
+				x[n] = x[n + 1];
+				x[n + 1] = y;
+			}
+		}
+	}
+	auto end = std::chrono::steady_clock::now();
+	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+	std::cout << elapsed_ms.count() << " ";
+}
+
+int main() {
+	for (int SIZE = 1000; SIZE <= 100000; SIZE += 100) {
+		int* x = new int[SIZE];
+		for (int i = 0; i < SIZE; i++) { //заполнение массива
+			x[i] = rand() % 100;
+			//cout << x[i];
+		}
+		constexpr int size = sizeof(x) / sizeof(int);
+		bubbleSort(x, SIZE);
+	}
+}
